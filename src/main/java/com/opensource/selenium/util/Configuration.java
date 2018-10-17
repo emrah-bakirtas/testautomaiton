33a198e93de167d7d5bbc9509fda6eb7b1946ac5
212688015 <emrah.bakirtas@ge.com>
2018-10-17 14:15:08 +0300

    Small things

--- src/main/java/com/opensource/selenium/util/Configuration.java
+++ src/main/java/com/opensource/selenium/util/Configuration.java
@@ -6,78 +6,61 @@ import java.util.Properties;
 
 public class Configuration {
 
-	private static Configuration instance;
-	
-	private Properties configProps = new Properties();
-	
-	private String appUrl;
-	private String excelInputPath;
-	private String excelOutputPath;
-	private String runSheetName;
-	private String resultSheetName;
-	private String chromedriverPath;
+    private static Configuration instance = new Configuration();
 
-	public static Configuration getInstance() {
-		
-		if(instance == null) {
-			
-			createInstance();
-		}
-		
-		return instance;
-	}
-	
-	private static synchronized void createInstance() {
-		
-		if(instance == null) {
-			
-			instance = new Configuration();
-		}
-	}
-	
-	private Configuration() {
-		
-		InputStream is = null;
-		try {
-			
-			is = ClassLoader.getSystemResourceAsStream("config.properties");
-			configProps.load(is);
-			
-			this.appUrl = configProps.getProperty("app.url");
-			this.excelInputPath = configProps.getProperty("excel.input.path");
-			this.excelOutputPath = configProps.getProperty("excel.output.path");
-			this.runSheetName = configProps.getProperty("run.sheet.name");
-			this.resultSheetName = configProps.getProperty("result.sheet.name");
-			this.chromedriverPath = configProps.getProperty("chromedriver.path");
+    private Properties configProps = new Properties();
 
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-		} finally {
-			
-			if(is != null) {
-				try {
-					is.close();
-				} catch (IOException e) {}
-			}
-		}
-	}
+    private String appUrl;
+    private String excelInputPath;
+    private String excelOutputPath;
+    private String runSheetName;
+    private String resultSheetName;
+    private String chromedriverPath;
 
-	public String getAppUrl() {
-		return appUrl;
-	}
+    public static Configuration getInstance() {
+        return instance;
+    }
 
-	public String getExcelInputPath() {
-		return excelInputPath;
-	}
+    private Configuration() {
 
-	public String getExcelOutputPath() {
-		return excelOutputPath;
-	}
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("config.properties");
+            configProps.load(is);
 
-	public String getRunSheetName(){ return runSheetName; }
+            this.appUrl = configProps.getProperty("app.url");
+            this.excelInputPath = configProps.getProperty("excel.input.path");
+            this.excelOutputPath = configProps.getProperty("excel.output.path");
+            this.runSheetName = configProps.getProperty("run.sheet.name");
+            this.resultSheetName = configProps.getProperty("result.sheet.name");
+            this.chromedriverPath = configProps.getProperty("chromedriver.path");
 
-	public String getResultSheetName(){ return resultSheetName; }
+        } catch (Exception e) {
 
-	public String getChromedriverPath(){ return chromedriverPath; }
+            e.printStackTrace();
+        }
+    }
+
+    public String getAppUrl() {
+        return appUrl;
+    }
+
+    public String getExcelInputPath() {
+        return excelInputPath;
+    }
+
+    public String getExcelOutputPath() {
+        return excelOutputPath;
+    }
+
+    public String getRunSheetName() {
+        return runSheetName;
+    }
+
+    public String getResultSheetName() {
+        return resultSheetName;
+    }
+
+    public String getChromedriverPath() {
+        return chromedriverPath;
+    }
 }
